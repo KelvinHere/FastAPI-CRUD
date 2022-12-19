771f32a21e287f3974cf4266ea4f808b6cefaacb
kelvinhere <kwardeu@gmail.com>
2022-12-19 11:52:44 +0000

    MINOR: Add metadata to apps and routes

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI, Depends
-import schemas
-import models
-
 from database import Base, engine, SessionLocal
 from sqlalchemy.orm import Session
+
+import tags
+import schemas
+import models
 
 # Create database using the config created in database.py if not exists
 Base.metadata.create_all(engine)
@@ -17,17 +18,15 @@ def get_session():
         session.close
 
 # Create FastApi app
-app = FastAPI()
-
-mockDB = {
-    1:{'task':'Fix car', 'completed': False},
-    2:{'task':'Clean loft', 'completed': False},
-    3:{'task':'Learn FastAPI basics', 'completed': False}
-}
+app = FastAPI(
+        openapi_tags=tags.tags_metadata,
+        title="ToDo",
+        description=tags.app_description
+        )
 
 
 # Home shows all items in DB
-@app.get("/")
+@app.get("/", tags=["home"])
 def home(session: Session = Depends(get_session)):
     items = session.query(models.Item).all()
     session.close()
@@ -35,16 +34,16 @@ def home(session: Session = Depends(get_session)):
 
 
 # Get an individual item by ID
-@app.get("/{id}")
-def getItem(id:int, session: Session = Depends(get_session)):
+@app.get("/{id}", tags=["get_item"])
+def get_item(id:int, session: Session = Depends(get_session)):
     item = session.query(models.Item).get(id)
     session.close()
     return item
 
 
 # Create a new item using Item schema
-@app.post("/")
-def addItem(item:schemas.Item, session: Session = Depends(get_session)):
+@app.post("/", tags=["post_item"])
+def add_item(item:schemas.Item, session: Session = Depends(get_session)):
     item = models.Item(task = item.task, completed = item.completed)
     session.add(item)
     session.commit()
@@ -53,8 +52,8 @@ def addItem(item:schemas.Item, session: Session = Depends(get_session)):
 
 
 # Update item by ID
-@app.put("/{id}")
-def updateItem(id:int, itemUpdated:schemas.Item, session: Session = Depends(get_session)):
+@app.put("/{id}", tags=["update_item"])
+def update_item(id:int, itemUpdated:schemas.Item, session: Session = Depends(get_session)):
     item = session.query(models.Item).get(id)
     item.task = itemUpdated.task
     item.completed = itemUpdated.completed
@@ -64,8 +63,8 @@ def updateItem(id:int, itemUpdated:schemas.Item, session: Session = Depends(get_
 
 
 # Delete item by ID
-@app.delete("/{id}")
-def updateItem(id:int, session: Session = Depends(get_session)):
+@app.delete("/{id}", tags=["delete_item"])
+def delete_item(id:int, session: Session = Depends(get_session)):
     item = session.query(models.Item).get(id)
     session.delete(item)
     session.commit()
---
+++ tags.py
@@ -0,0 +1,35 @@
+tags_metadata = [
+    {
+        "name": "home",
+        "description": "Home displays all current tasks"
+    },
+    {   "name": "get_item",
+        "description": "Retrieve an item by ID"
+    },
+    {   "name": "post_item",
+        "description": "Creates a new task & inserts to DB"
+    },
+    {   "name": "update_item",
+        "description": "Updates an item by ID"
+    },
+    {   "name": "delete_item",
+        "description": "Deletes item by ID"
+    },
+]
+
+app_description = """ 
+A simple ToDo app using fast API and SqlAlchemy
+
+## Users can
+* Retrieve all tasks
+* Retrieve a single task
+* Create a task
+* Update a task
+* Delete a task
+
+## To be implemented
+
+* User can mark a task complete
+
+
+"""
