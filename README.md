1b0f1ad354f2a1012415ba2b0acb8b1d53955660
kelvinhere <kwardeu@gmail.com>
2022-12-19 10:30:57 +0000

    FEAT: Add routes to create and retrieve items

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # FastAPI-CRUD
 Learning FastAPI &amp; implementing simple CRUD
+
+
+## Use
+
+/docs gives access to interface to test APIs
--- main.py
+++ main.py
@@ -1,7 +1,33 @@
 from fastapi import FastAPI
+import schemas
 
 app = FastAPI()
 
+mockDB = {
+    1:{'task':'Fix car', 'completed': False},
+    2:{'task':'Clean loft', 'completed': False},
+    3:{'task':'Learn FastAPI basics', 'completed': False}
+}
+
+
+# Home shows all items in DB
 @app.get("/")
-async def home():
-    return {"a": "b"}
+def home():
+    return mockDB
+
+
+# Get an individual item by ID
+@app.get("/{id}")
+def getItem(id:int):
+    return mockDB[id]
+
+
+# Create a new item using Item schema
+@app.post("/")
+def addItem(item:schemas.Item):
+    newId = len(mockDB) + 1
+    mockDB[newId] = {
+        'task': item.task,
+        'completed': item.completed
+        }
+    return mockDB
